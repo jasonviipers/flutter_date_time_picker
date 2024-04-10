9dc9edc931a2b784c699416aa243a51da33e2de5
vii <vii@viis-Mac-Pro.local>
2024-04-10 19:22:17 +0200

    updat

--- README.md
+++ README.md
@@ -1,39 +1,96 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# date_time_picker
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
+[![pub package](https://img.shields.io/pub/v/date_time_picker.svg)](https://pub.dartlang.org/packages/date_time_picker)
 
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
-
-## Features
-
-TODO: List what your package can do. Maybe include images, gifs, or videos.
-
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+A Flutter widget to show a text form field to display a date or clock dialog.\
+This widget extend TextField and has a similar behavior as TextFormField
 
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
+In the `pubspec.yaml` of your flutter project, add the following dependency:
 
-```dart
-const like = 'sample';
+```yaml
+dependencies:
+  ...
+  date_time_picker: "^2.1.0"
 ```
 
-## Additional information
+In your library add the following import:
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+```dart
+import 'package:date_time_picker/date_time_picker.dart';
+```
+
+For help getting started with Flutter, view the online [documentation](https://flutter.io/).
+
+## Example
+
+There are four presentations for DateTimePicker and can be defined in the type parameter:
+* `DateTimePickerType.date` will present a text field with the action tap showing a datePicker dialog box;
+* `DateTimePickerType.time` will present a text field with the action tap showing a timePicker dialog box;
+* `DateTimePickerType.dateTime` will present a text field with the action tap showing a datePicker dialog box then a timePicker dialog box;
+* `DateTimePickerType.dateTimeSeparated` will display two text fields side by side, the first for date and the second for time. Each displaying their respective dialog box, datePicker and timePicker in the tap action;
+  
+``` dart
+DateTimePicker(
+  type: date, // options: [date | time | dateTime | dateTimeSeparated], default is date
+  ...
+)
+```
+
+initialValue or controller.text can be `null`, `empty` or a `DateTime string` otherwise it will throw an error.
+
+``` dart
+DateTimePicker(
+  initialValue: '',
+  firstDate: DateTime(2000),
+  lastDate: DateTime(2100),
+  dateLabelText: 'Date',
+  onChanged: (val) => print(val),
+  validator: (val) {
+    print(val);
+    return null;
+  },
+  onSaved: (val) => print(val),
+);
+```
+
+More complete example:
+
+
+``` dart
+DateTimePicker(
+  type: DateTimePickerType.dateTimeSeparate,
+  dateMask: 'd MMM, yyyy',
+  initialValue: DateTime.now().toString(),
+  firstDate: DateTime(2000),
+  lastDate: DateTime(2100),
+  icon: Icon(Icons.event),
+  dateLabelText: 'Date',
+  timeLabelText: "Hour",
+  selectableDayPredicate: (date) {
+    // Disable weekend days to select from the calendar
+    if (date.weekday == 6 || date.weekday == 7) {
+      return false;
+    }
+
+    return true;
+  },
+  onChanged: (val) => print(val),
+  validator: (val) {
+    print(val);
+    return null;
+  },
+  onSaved: (val) => print(val),
+);
+```
+
+The result of val in `onChanged`, `validator` and `onSaved` will be a DateTime String or just a Time String:
+* ex.: [2020-07-20 14:30] or [15:30] DateTimePickerType.time;
+* month, day, hour and minute will be 2 digits and time always be in 24 hours mode;
+* but the presentation in text field can be formated by the dateMask parameter.
+
+
+## Preview
+![Overview](https://raw.githubusercontent.com/m3uzz/date_time_picker/master/doc/images/date_time_picker.gif)
--- lib/flutter_date_time_picker.dart
+++ lib/flutter_date_time_picker.dart
@@ -1,7 +1,165 @@
 library flutter_date_time_picker;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+enum DateTimePickerType { date, time, dateTime, dateTimeSeparate }
+
+class DateTimePicker extends StatefulWidget {
+  const DateTimePicker({
+    Key? key,
+    required this.type,
+    required this.onChanged,
+    this.firstDate,
+    this.lastDate,
+    this.initialDate,
+    this.initialTime,
+    this.dateMask,
+    this.icon,
+    this.dateLabelText,
+    this.timeLabelText,
+    this.dateHintText,
+    this.timeHintText,
+    this.calendarTitle,
+    this.cancelText,
+    this.confirmText,
+    this.fieldLabelText,
+    this.fieldHintText,
+    this.errorFormatText,
+    this.errorInvalidText,
+    this.initialEntryMode,
+    this.initialDatePickerMode,
+    this.selectableDayPredicate,
+    this.textDirection,
+    this.locale,
+    this.useRootNavigator = false,
+    this.routeSettings,
+    this.use24HourFormat = true,
+    this.timeFieldWidth,
+    this.timePickerEntryModeInput = false,
+  }) : super(key: key);
+
+  final DateTimePickerType type;
+  final ValueChanged<String> onChanged;
+  final DateTime? firstDate;
+  final DateTime? lastDate;
+  final DateTime? initialDate;
+  final TimeOfDay? initialTime;
+  final String? dateMask;
+  final Widget? icon;
+  final String? dateLabelText;
+  final String? timeLabelText;
+  final String? dateHintText;
+  final String? timeHintText;
+  final String? calendarTitle;
+  final String? cancelText;
+  final String? confirmText;
+  final String? fieldLabelText;
+  final String? fieldHintText;
+  final String? errorFormatText;
+  final String? errorInvalidText;
+  final DatePickerEntryMode? initialEntryMode;
+  final DatePickerMode? initialDatePickerMode;
+  final SelectableDayPredicate? selectableDayPredicate;
+  final TextDirection? textDirection;
+  final Locale? locale;
+  final bool useRootNavigator;
+  final RouteSettings? routeSettings;
+  final bool use24HourFormat;
+  final double? timeFieldWidth;
+  final bool timePickerEntryModeInput;
+
+  @override
+  _DateTimePickerState createState() => _DateTimePickerState();
+}
+
+class _DateTimePickerState extends State<DateTimePicker> {
+  DateTime? _selectedDate;
+  TimeOfDay? _selectedTime;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDate = widget.initialDate;
+    _selectedTime = widget.initialTime;
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate ?? DateTime.now(),
+      firstDate: widget.firstDate ?? DateTime.now(),
+      lastDate: widget.lastDate ?? DateTime.now(),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+      widget.onChanged(DateFormat.yMd().format(picked));
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime ?? TimeOfDay.now(),
+    );
+    if (picked != null && picked != _selectedTime) {
+      setState(() {
+        _selectedTime = picked;
+      });
+      widget.onChanged(MaterialLocalizations.of(context).formatTimeOfDay(picked));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: InkWell(
+            onTap: () => _selectDate(context),
+            child: IgnorePointer(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: widget.dateLabelText,
+                  hintText: widget.dateHintText,
+                  icon: widget.icon,
+                ),
+                controller: TextEditingController(
+                  text: _selectedDate == null
+                      ? null
+                      : DateFormat.yMd().format(_selectedDate!),
+                ),
+              ),
+            ),
+          ),
+        ),
+        if (widget.type == DateTimePickerType.dateTimeSeparate)
+          const SizedBox(width: 15),
+        if (widget.type == DateTimePickerType.dateTimeSeparate)
+          SizedBox(
+            width: widget.timeFieldWidth ?? 100,
+            child: InkWell(
+              onTap: () => _selectTime(context),
+              child: IgnorePointer(
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: widget.timeLabelText,
+                    hintText: widget.timeHintText,
+                  ),
+                  controller: TextEditingController(
+                    text: _selectedTime == null
+                        ? null
+                        : MaterialLocalizations.of(context)
+                            .formatTimeOfDay(_selectedTime!),
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
 }
